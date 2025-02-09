d1034ea10c1af7f801ee42de7f0611069c3ffb4a
kj-art-dev <56623499+kj-art-dev@users.noreply.github.com>
2025-02-09 11:33:39 -0800

    Update PasswordGeneratorTest.java
    Checking in

--- src/test/java/net/rptools/maptool/util/PasswordGeneratorTest.java
+++ src/test/java/net/rptools/maptool/util/PasswordGeneratorTest.java
@@ -1,49 +1,61 @@
+/*
+ * This software Copyright by the RPTools.net development team, and
+ * licensed under the Affero GPL Version 3 or, at your option, any later
+ * version.
+ *
+ * MapTool Source Code is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License * along with this source Code.  If not, please visit
+ * <http://www.gnu.org/licenses/> and specifically the Affero license
+ * text at <http://www.gnu.org/licenses/agpl.html>.
+ */
 package net.rptools.maptool.util;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
-
-import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
 import java.util.Set;
+import org.junit.jupiter.api.Test;
 
 /**
  * @kj-art-dev: Doing some basic testing here.
  */
 public class PasswordGeneratorTest {
 
-    private final PasswordGenerator testPasswordGenerator = new PasswordGenerator();
+  private final PasswordGenerator testPasswordGenerator = new PasswordGenerator();
 
-    private static final String ELIGIBLE_CHARACTERS =
-            "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWZY23456789+-@#!";
+  private static final String ELIGIBLE_CHARACTERS =
+      "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWZY23456789+-@#!";
 
-    @Test
-    void testGeneratePasswordWithValidRange() {
-        String testGeneratedPassword = testPasswordGenerator.getPassword();
+  @Test
+  void testGeneratePasswordWithValidRange() {
+    String testGeneratedPassword = testPasswordGenerator.getPassword();
 
-        int actualPasswordLength = testGeneratedPassword.length();
+    int actualPasswordLength = testGeneratedPassword.length();
 
-        assertTrue(actualPasswordLength >= 15 && testGeneratedPassword.length() <= 30);
+    assertTrue(actualPasswordLength >= 15 && testGeneratedPassword.length() <= 30);
+  }
+
+  @Test
+  void testGeneratePasswordWithValidCharacters() {
+    String testGeneratedPassword = testPasswordGenerator.getPassword();
+
+    for (char c : testGeneratedPassword.toCharArray()) {
+      assertTrue(ELIGIBLE_CHARACTERS.indexOf(c) >= 0);
     }
+  }
 
-    @Test
-    void testGeneratePasswordWithValidCharacters() {
-        String testGeneratedPassword = testPasswordGenerator.getPassword();
+  @Test
+  void testGeneratePasswordsThatAreUnique() {
+    Set<String> generatedPasswords = new HashSet<>();
+    int generatedPasswordCount = 100;
 
-        for (char c : testGeneratedPassword.toCharArray()) {
-            assertTrue(ELIGIBLE_CHARACTERS.indexOf(c) >= 0);
-        }
+    for (int i = 0; i < generatedPasswordCount; i++) {
+      generatedPasswords.add(testPasswordGenerator.getPassword());
     }
-
-    @Test
-    void testGeneratePasswordsThatAreUnique() {
-        Set<String> generatedPasswords = new HashSet<>();
-        int generatedPasswordCount = 100;
-
-        for (int i = 0; i < generatedPasswordCount; i++) {
-            generatedPasswords.add(testPasswordGenerator.getPassword());
-        }
-        assertEquals(generatedPasswordCount, generatedPasswords.size());
-    }
+    assertEquals(generatedPasswordCount, generatedPasswords.size());
+  }
 }
