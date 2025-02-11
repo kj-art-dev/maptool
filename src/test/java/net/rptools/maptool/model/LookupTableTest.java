e09c20784f1a4af04f2bb0d3b64b01439fb1c463
kj-art-dev <56623499+kj-art-dev@users.noreply.github.com>
2025-02-10 21:25:51 -0800

    Update LookupTableTest.java
    Added some new unit tests for practice.

--- src/test/java/net/rptools/maptool/model/LookupTableTest.java
+++ src/test/java/net/rptools/maptool/model/LookupTableTest.java
@@ -2,11 +2,14 @@ package net.rptools.maptool.model;
 
 import net.rptools.lib.MD5Key;
 import net.rptools.maptool.client.MapTool;
+import net.rptools.maptool.server.proto.LookupEntryDto;
+import net.rptools.maptool.server.proto.LookupTableDto;
 import net.rptools.parser.ParserException;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.random.*;
 
@@ -16,13 +19,13 @@ public class LookupTableTest {
 
 
     @Test
-    public void testShouldCreateLookupTableBaseLine() {
+    public void testShouldCreateValidLookupTable() {
         LookupTable lookupTableTest = new LookupTable();
         assertNotNull(lookupTableTest);
     }
 
     @Test
-    public void testShouldSetTableImage() {
+    public void testShouldGetValidTableImage() {
         LookupTable lookupTableTest = new LookupTable();
         MD5Key imageKeyTest = new MD5Key(new File("src/test/resources/circleToken.png").getAbsolutePath().getBytes());
 
@@ -30,6 +33,26 @@ public class LookupTableTest {
         MD5Key actualImageKey = lookupTableTest.getTableImage();
 
         assertEquals(imageKeyTest, actualImageKey);
+    }
+
+    @Test
+    public void testTablesShouldPickOnce() {
+        int tableAmountTest = 25;
+        LookupTable[] tablesTest = new LookupTable[tableAmountTest];
+
+        for (int i = 0; i < tableAmountTest; i++) {
+            LookupTable lookupTableTest = new LookupTable();
+            lookupTableTest.setPickOnce(Math.random() < 0.5);
+            tablesTest[i] = lookupTableTest;
+        }
+
+        for (LookupTable lookupTableTest : tablesTest) {
+            if (lookupTableTest.getPickOnce()) {
+                assertTrue(lookupTableTest.getPickOnce());
+            } else {
+                assertFalse(lookupTableTest.getPickOnce());
+            }
+        }
     }
 
     @Test
@@ -68,7 +91,7 @@ public class LookupTableTest {
     }
 
     @Test
-    public void testShouldGetCorrectTableEntryFromRandomRollsWithinRange() throws ParserException {
+    public void testShouldGetLookupFromRandomRollsWithinRange() throws ParserException {
         LookupTable lookupTableTest = new LookupTable();
         MD5Key imageKeyTest = new MD5Key(new File("src/test/resources/circleToken.png").getAbsolutePath().getBytes());
         int rollCount = 50;
@@ -101,6 +124,39 @@ public class LookupTableTest {
     }
 
     @Test
+    public void testShouldGetLookupDirectFromRandomRollsWithinRange() throws ParserException {
+        LookupTable lookupTableTest = new LookupTable();
+        MD5Key imageKeyTest = new MD5Key(new File("src/test/resources/circleToken.png").getAbsolutePath().getBytes());
+        int rollCount = 50;
+
+        lookupTableTest.addEntry(1, 5, "entry1", imageKeyTest);
+        lookupTableTest.addEntry(6, 10, "entry2", imageKeyTest);
+        lookupTableTest.addEntry(11, 20, "entry3", imageKeyTest);
+
+        String expectedEntry1 = "entry1";
+        String expectedEntry2 = "entry2";
+        String expectedEntry3 = "entry3";
+
+        for (int i = 0; i < rollCount; i++) {
+
+            int randomRoll = ThreadLocalRandom.current().nextInt(1, 20);
+            String actualEntry = lookupTableTest.getLookupDirect(Integer.toString(randomRoll)).getValue();
+
+            if (randomRoll >= 1 && randomRoll < 6) {
+                assertEquals(expectedEntry1, actualEntry);
+            }
+
+            if (randomRoll >= 6 && randomRoll < 11) {
+                assertEquals(expectedEntry2, actualEntry);
+            }
+
+            if (randomRoll >= 11 && randomRoll < 21) {
+                assertEquals(expectedEntry3, actualEntry);
+            }
+        }
+    }
+
+    @Test
     public void testLookupTableNameLengthValidation() {
         LookupTable lookupTableTest1 = new LookupTable();
         lookupTableTest1.setName("");
@@ -114,10 +170,10 @@ public class LookupTableTest {
         LookupTable lookupTableTest4 = new LookupTable();
         lookupTableTest4.setName("abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWZY23456789+-@#!");
 
-        assertFalse(lookupTableTest1.getName().trim().length() > 0);
-        assertFalse(lookupTableTest2.getName().trim().length() > 0);
-        assertTrue(lookupTableTest3.getName().trim().length() > 0);
-        assertTrue(lookupTableTest4.getName().trim().length() > 0);
+        assertTrue(lookupTableTest1.getName().trim().isEmpty());
+        assertTrue(lookupTableTest2.getName().trim().isEmpty());
+        assertFalse(lookupTableTest3.getName().trim().isEmpty());
+        assertFalse(lookupTableTest4.getName().trim().isEmpty());
     }
 
     @Test
@@ -136,6 +192,38 @@ public class LookupTableTest {
         lookupTableTest.clearEntries();
 
         assertEquals(0, lookupTableTest.getEntryList().size());
+    }
 
+    @Test
+    public void testShouldConvertLookupTableToDtoAndBackAgain()
+    {
+        LookupTable lookupTableTest = new LookupTable();
+
+        LookupTableDto lookupTableDtoTest = lookupTableTest.toDto();
+
+        assertNotNull(lookupTableDtoTest);
+        assertInstanceOf(LookupTableDto.class, lookupTableDtoTest);
+
+        LookupTable lookupTableFromDto = LookupTable.fromDto(lookupTableDtoTest);
+        assertNotNull(lookupTableFromDto);
+        assertInstanceOf(LookupTable.class, lookupTableFromDto);
+    }
+
+    @Test
+    public void testShouldConvertLookupEntryToDtoAndBackAgain()
+    {
+        MD5Key imageKeyTest = new MD5Key(new File("src/test/resources/circleToken.png").getAbsolutePath().getBytes());
+
+        LookupTable.LookupEntry lookupEntryTest = new LookupTable.LookupEntry(1, 5, "testEntry", imageKeyTest);
+
+        LookupEntryDto lookupEntryDtoTest = lookupEntryTest.toDto();
+
+        assertNotNull(lookupEntryDtoTest);
+        assertInstanceOf(LookupEntryDto.class, lookupEntryDtoTest);
+
+        LookupTable.LookupEntry actualLookupEntry = LookupTable.LookupEntry.fromDto(lookupEntryDtoTest);
+
+        assertNotNull(actualLookupEntry);
+        assertInstanceOf(LookupTable.LookupEntry.class, actualLookupEntry);
     }
 }
